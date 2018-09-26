bb147f98b06a0e42d78404161c825f0120a65337
Prashant.Mishra <prashant.cse2007@gmail.com>
2018-09-26 09:36:02 +0530

    //Created packages and added dependencies.

--- app/src/main/java/com/prashant/demowipro/view/MainActivity.java
+++ app/src/main/java/com/prashant/demowipro/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.prashant.demowipro;
+package com.prashant.demowipro.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.prashant.demowipro.R;
 
 public class MainActivity extends AppCompatActivity {
 
